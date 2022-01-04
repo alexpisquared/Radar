0691f049028985a9654463956cfd4d6877897048
AlexPi <alex.pigida@outlook.com>
2022-01-04 11:08:01 -0500

    SUCCESS on JSON current load to C# types!!!

--- Src/Radar/Logic/OpenWeatherRevisit2022.cs
+++ Src/Radar/Logic/OpenWeatherRevisit2022.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Radar.JsonResults;
+using Radar.JsonResults.XML;
 using Radar.Logic.Forecast;
 
 namespace Radar.Logic;
@@ -17,30 +19,34 @@ internal class OpenWeatherRevisit2022
     string time = "1586468027",
     string frmt = "json") // XML gives readable time for sunrise!!!
   {
-    //double x = 1, y = 1, z = 1; //
-    double lat = -79.4829, lon = 43.8001, z = 1;
+    try
+    {
+      //double x = 1, y = 1, z = 1; //
+      double lat = -79.4829, lon = 43.8001, z = 1;
 
-    var url = /*
+      var url = /*
       $"http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={code}";        <== needs $ subs-n ti seemes.
       $"https://pro.openweathermap.org/data/2.5/forecast/climate?q=London&appid={code}"; // https://openweathermap.org/api/forecast30 //  <== needs $ subs-n ti seemes.
       $"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={code}";      // https://openweathermap.org/api/one-call-api --5 days back only.
-      $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&mode={mode}&appid={code}";              // https://openweathermap.org/current       */
       $"https://api.openweathermap.org/data/2.5/forecast/daily?q={city}&units=metric&mode={frmt}{xtra}&appid={code}"; // https://openweathermap.org/forecast16      
+      */
+        $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&mode={frmt}&appid={code}";              // https://openweathermap.org/Rootobject       
 
-    //http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={API key}
-
-    HttpClient _cf = new();
-    var response = await _cf.GetAsync(url);
-    if (response == null || response.StatusCode == System.Net.HttpStatusCode.NotFound) return null;
+      HttpClient _cf = new();
+      var response = await _cf.GetAsync(url);
+      if (response == null || response.StatusCode == System.Net.HttpStatusCode.NotFound) return null;
 
 #if !true
     var json = await response.Content.ReadAsStringAsync();
     Trace.WriteLine(json);
-    //await File.WriteAllTextAsync($@"..\..\..\JsonResults\{city}-{xtra}-{DateTime.Now:yyMMdd·HHmmss}.{frmt}", json);
+    await File.WriteAllTextAsync($@"..\..\..\JsonResults\{city}-{xtra}-{DateTime.Now:yyMMdd·HHmmss}.{frmt}", json);
+    return new Rootobject();
 #else
-    object? weathe2 = await response.Content.ReadFromJsonAsync(typeof(object)) ;
-    var weather = await response.Content.ReadFromJsonAsync<Rootobject>(); //todo: https://docs.microsoft.com/en-us/aspnet/core/web-api/route-to-code?view=aspnetcore-6.0
-    return weather; // return new Rootobject();
+      //object? weathe2 = await response.Content.ReadFromJsonAsync(typeof(object)) ;
+      var weather = await response.Content.ReadFromJsonAsync<Rootobject>(); //todo: https://docs.microsoft.com/en-us/aspnet/core/web-api/route-to-code?view=aspnetcore-6.0
+      return weather;
 #endif
+    }
+    catch (Exception ex) { Trace.WriteLine(ex); throw; }
   }
 }
