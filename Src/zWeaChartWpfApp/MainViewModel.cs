c7fab15e323d88f9e2d76273ee4fa9d335448d12
AlexPi <alex.pigida@outlook.com>
2022-01-10 15:02:29 -0500

    +++

--- Src/zWeaChartWpfApp/MainViewModel.cs
+++ Src/zWeaChartWpfApp/MainViewModel.cs
@@ -10,58 +10,85 @@ namespace zWeaChartWpfApp;
 
 public class MainViewModel
 {
+  IConfigurationRoot _config;
+  private OpenWeatherRevisit2022 _opnwea;
+
   public MainViewModel()
   {
-    ScatModel = new PlotModel { Title = "ScatterSeries" };
-    var scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
-    var r = new Random(314);
-    for (int i = 0; i < 100; i++)
-    {
-      var x = r.NextDouble();
-      var y = r.NextDouble();
-      var size = r.Next(5, 15);
-      var colorValue = r.Next(100, 1000);
-      scatterSeries.Points.Add(new ScatterPoint(x, y, size, colorValue));
-    }
-
-    ScatModel.Series.Add(scatterSeries);
-    //ScatModel.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Jet(200) });
-
-
-    FuncModel = new PlotModel { Title = "FunctionSeries Example" };
-    FuncModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
-
-    Title = "Example 2";
-
-
-    PopulateAsync();
+    _config = new ConfigurationBuilder().AddUserSecrets<App>().Build(); //tu: adhoc usersecrets 
+    _opnwea = new OpenWeatherRevisit2022();
   }
 
-  async void PopulateAsync()
+  public async Task<bool> PopulateAsync()
   {
-    var config = new ConfigurationBuilder().AddUserSecrets<App>().Build(); //tu: adhoc usersecrets 
-    WriteLine($"---   WhereAmI: '{config["WhereAmI"]}'       {config["AppSecrets:MagicNumber"]}");
+    await Task.Yield();
+    await
+    PopulateScatModelAsync();
+    await PopulateFuncModel();
+    await PopulateOpenWeath();
+    return true;
+  }
 
-    var oo = new OpenWeatherRevisit2022();
-    var occ = await oo.GetIt(config["AppSecrets:MagicNumber"], 43.8374229, -79.4961442); // PHC107
-    var oct = await oo.GetIt(config["AppSecrets:MagicNumber"], 43.7181557, -79.5181414); // 400x401
+  async Task PopulateFuncModel()
+  {
+    await Task.Yield();
+    FuncModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+  }
+
+  async Task PopulateScatModelAsync()
+  {
+    var ocv = await _opnwea.GetIt(_config["AppSecrets:MagicNumber"], 43.8374229, -79.4961442); // PHC107
+    ArgumentNullException.ThrowIfNull(ocv);
+
+    var scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
+
+    var minValue = DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(ocv.hourly.Min(d => d.dt - 3600)));
+    var maxValue = DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(ocv.daily.Max(d => d.dt + 3600)));
+
+    ScatModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, Minimum = minValue, Maximum = maxValue, StringFormat = "ddd HH" });
+
+    ocv.hourly.ToList().ForEach(x =>
+    {
+      scatterSeries.Points.Add(new ScatterPoint(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt)), x.temp, 6, 630));
+      scatterSeries.Points.Add(new ScatterPoint(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt)), x.feels_like, 3, 530));
+    });
+
+    ocv.daily./*Where(d => d.dt > ocv.hourly.Max(d => d.dt)).*/ToList().ForEach(x =>
+    {
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt - 08 * 3600)), x.temp.morn, 4, 0));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 00 * 3600)), x.temp.day, 4, 750));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 06 * 3600)), x.temp.eve, 4, 1000));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 11 * 3600)), x.temp.night, 4, 333));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt - 08 * 3600)), x.feels_like.morn, 2, 0));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 00 * 3600)), x.feels_like.day, 2, 750));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 06 * 3600)), x.feels_like.eve, 2, 1000));
+      scatterSeries.Points.Add(new(DateTimeAxis.ToDouble(OpenWeatherRevisit2022.UnixTimeStampToDateTime(x.dt + 11 * 3600)), x.feels_like.night, 2, 333));
+    });
+
+    ScatModel.Series.Add(scatterSeries);
+    ScatModel.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Rainbow(1000) });
+  }
+
+  async Task PopulateOpenWeath()
+  {
+    var occ = await _opnwea.GetIt(_config["AppSecrets:MagicNumber"], 43.8374229, -79.4961442); // PHC107
+    var oct = await _opnwea.GetIt(_config["AppSecrets:MagicNumber"], 43.7181557, -79.5181414); // 400x401
 
     DrawSeries(occ, PointsTC, PointsFC);
     DrawSeries(oct, PointsTT, PointsFT);
-    
   }
 
   void DrawSeries(RootobjectOneCallApi? ocv, ObservableCollection<DataPoint> pointsTemp, ObservableCollection<DataPoint> pointsFeel)
   {
     ArgumentNullException.ThrowIfNull(ocv);
 
-    ocv?.hourly.ToList().ForEach(x =>
+    ocv.hourly.ToList().ForEach(x =>
     {
       pointsTemp.Add(new(x.dt, x.temp));
       pointsFeel.Add(new(x.dt, x.feels_like));
     });
 
-    ocv?.daily.Where(d => d.dt > ocv.hourly.Max(d => d.dt)).ToList().ForEach(x =>
+    ocv.daily.Where(d => d.dt > ocv.hourly.Max(d => d.dt)).ToList().ForEach(x =>
     {
       pointsTemp.Add(new(x.dt + 06 * 3600, x.temp.morn));
       pointsTemp.Add(new(x.dt + 12 * 3600, x.temp.day));
@@ -74,7 +101,7 @@ public class MainViewModel
     });
   }
 
-  public string Title { get; private set; }
+  public string Title { get; private set; } = "Example 2";
   public ObservableCollection<DataPoint> PointsTV { get; private set; } = new ObservableCollection<DataPoint>();
   public ObservableCollection<DataPoint> PointsFV { get; private set; } = new ObservableCollection<DataPoint>();
   public ObservableCollection<DataPoint> PointsTC { get; private set; } = new ObservableCollection<DataPoint>();
@@ -82,8 +109,8 @@ public class MainViewModel
   public ObservableCollection<DataPoint> PointsTT { get; private set; } = new ObservableCollection<DataPoint>();
   public ObservableCollection<DataPoint> PointsFT { get; private set; } = new ObservableCollection<DataPoint>();
 
-  public PlotModel FuncModel { get; private set; }
-  public PlotModel ScatModel { get; private set; }
+  public PlotModel FuncModel { get; private set; } = new PlotModel { Title = "FunctionSeries Example" };
+  public PlotModel ScatModel { get; private set; } = new PlotModel { Title = "ScatterSeries" };
 }
 ///todo: https://oxyplot.readthedocs.io/en/latest/models/series/ScatterSeries.html
 ///https://docs.microsoft.com/en-us/answers/questions/22863/how-to-customize-charts-in-wpf-using-systemwindows.html
--- Src/zWeaChartWpfApp/MainWindow.xaml.cs
+++ Src/zWeaChartWpfApp/MainWindow.xaml.cs
@@ -7,9 +7,16 @@ public partial class MainWindow : Window
   public MainWindow()
   {
     InitializeComponent();
-    DataContext = new MainViewModel();
   }
 
-
   void OnClose(object sender, RoutedEventArgs e) => Close();
+
+  async void OnLoadad(object sender, RoutedEventArgs e)
+  {
+    //await Task.Delay(100);
+    var ataContext = new MainViewModel();
+    bool rv = await ataContext.PopulateAsync();
+    var rrv = rv;
+    DataContext = ataContext;
+  }
 }
