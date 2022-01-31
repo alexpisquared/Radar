0861ef395bc997c458948fc9c3ad4484b14896ec
AlexPi <alex.pigida@outlook.com>
2022-01-30 22:18:56 -0500

    pau

--- Src/OpenWeaWpfApp/Views/MainWindow.xaml.cs
+++ Src/OpenWeaWpfApp/Views/MainWindow.xaml.cs
@@ -27,17 +27,14 @@ public partial class MainWindow : Window
       //Width = 3000;
       //Height = 1080;
     }
-    else
+    else if (Environment.UserDomainName == "RAZER1")
     {
-      if (Environment.UserDomainName == "RAZER1")
-      {
-        WindowState = WindowState.Normal;
-        WindowStartupLocation = WindowStartupLocation.Manual;
-        Left = 1920;
-        Top = -1120;
-        Width = 3000;
-        Height = 1080;
-      }
+      WindowState = WindowState.Normal;
+      WindowStartupLocation = WindowStartupLocation.Manual;
+      Left = 1920;
+      Top = -1120;
+      Width = 3000;
+      Height = 1080;
     }
   }
 
@@ -45,15 +42,23 @@ public partial class MainWindow : Window
 
   async void OnLoadad(object sender, RoutedEventArgs e)
   {
-    await Task.Delay(1);
-    Beep.Play();
+    try
+    {
+      await Task.Delay(1);
+      Beep.Play();
 
-    SctrPtTPFVgn.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
-    SctrPtTPFMis.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
-    SctrPtTPFPhc.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
+      SctrPtTPFVgn.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
+      SctrPtTPFMis.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
+      SctrPtTPFPhc.TrackerFormatString += "{Tag}"; // Oxyplot ScatterPoint custom Label
 
-    _ = await ((MainViewModel)DataContext).PopulateAsync();  // only lines chart is drawn.
-    Beep.Play();
+      _ = await ((MainViewModel)DataContext).PopulateAsync();  // only lines chart is drawn.
+      Beep.Play();
+    }
+    catch (Exception ex)
+    {
+      WriteLine($"@@@@@@@@ {ex.Message} \n\t {ex} @@@@@@@@@@");
+      if (Debugger.IsAttached) Debugger.Break(); else MessageBox.Show(ex.ToString(), ex.Message, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.ServiceNotification);
+    }
   }
 
   async void OnPoplte(object sender, RoutedEventArgs e) => _ = await ((MainViewModel)DataContext).PopulateAsync();  // only lines chart is drawn.
