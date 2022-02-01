4bf464256bc599ec88c5117f65b085acd7854eb6
AlexPi <alex.pigida@outlook.com>
2022-02-01 10:25:42 -0500

    old composite instaed of IR

--- Src/Radar/App.xaml.cs
+++ Src/Radar/App.xaml.cs
@@ -14,19 +14,7 @@ public partial class App : Application
 
   protected override async void OnStartup(StartupEventArgs e)
   {
-#if DEBUG
-    var config = new ConfigurationBuilder().AddUserSecrets<App>().Build(); //tu: adhoc usersecrets 
-    Trace.WriteLine($"---   WhereAmI: '{config["WhereAmI"]}'       {config["AppSecrets:MagicNumber"]}");
-
-    /* //tu: not storing file in the GitHub:     (https://youtu.be/ASraHYMi808?t=832)
-    git update-index --assume-unchanged AppSettings.json
-    git update-index --no-assume-unchanged AppSettings.json            */
-
-    var rv = await new OpenWeather2022.OpenWea().ParseJsonToClasses(config["AppSecrets:MagicNumber"]);
-    Current.Shutdown();
-#endif
-
-    base.OnStartup(e);      //
+    base.OnStartup(e);      
                             //Bpr.BeepBgn2();
 
     Current.DispatcherUnhandledException += UnhandledExceptionHndlr.OnCurrentDispatcherUnhandledException;     //new SpeechSynthesizer().Speak("Testing");			new SpeechSynthesizer().SpeakAsync("Testing");
