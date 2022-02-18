497e89a95187d2a0e8a7038da66818f427afc5b8
AlexPi <alex.pigida@outlook.com>
2022-02-18 13:22:27 -0500

    pau

--- Src/xEnvtCanRadar/Views/RadarTypeViewUserControl.xaml.cs
+++ Src/xEnvtCanRadar/Views/RadarTypeViewUserControl.xaml.cs
@@ -2,12 +2,16 @@
 
 public partial class RadarTypeViewUserControl : UserControl
 {
-  const string urlRoot = "https://dd.meteo.gc.ca/radar/";
+  const string _urlRoot = "https://dd.meteo.gc.ca/radar/";
   const double _fpsPeriod = .04;
+  const int _pauseFrames = 25;
+  const int _maxFrames = 25 * 60;
+  readonly CancellationTokenSource _cts = new();
+  bool _loaded;
 
   public RadarTypeViewUserControl() => InitializeComponent();
 
-  async void OnLoaded(object s, RoutedEventArgs e) => await ReLoad(31);
+  async void OnLoaded(object s, RoutedEventArgs e) { await ReLoad(31); _loaded = true; }
   async void OnReload(object s, RoutedEventArgs e) => await ReLoad(int.Parse(((FrameworkElement)s).Tag?.ToString() ?? "0")); // max is 480 == 2 days on 10 per hour basis.
   async Task ReLoad(int takeLastCount)
   {
@@ -16,13 +20,13 @@ public partial class RadarTypeViewUserControl : UserControl
 
     try
     {
-      var gifurls = await new WebDirectoryLoader().ParseFromHtmlUsingRegex($"{urlRoot}{UrlSuffix}", PreciTp, takeLastCount);
+      var gifurls = await new WebDirectoryLoader().ParseFromHtmlUsingRegex($"{_urlRoot}{UrlSuffix}", PreciTp, takeLastCount);
 
       var list = new List<RI>();
       lbxAllPics.Items.Clear();
 
-      gifurls.ForEach(imgFile => /**/ list.Add(new RI { GifUrl = $"{urlRoot}{UrlSuffix}/{imgFile}", FileName = Path.GetFileNameWithoutExtension(imgFile) }));
-      gifurls.ForEach(imgFile => lbxAllPics.Items.Add(new RI { GifUrl = $"{urlRoot}{UrlSuffix}/{imgFile}", FileName = Path.GetFileNameWithoutExtension(imgFile) }));      //lbxAllPics.ItemsSource = list;
+      gifurls.ForEach(imgFile => /**/ list.Add(new RI { GifUrl = $"{_urlRoot}{UrlSuffix}/{imgFile}", FileName = Path.GetFileNameWithoutExtension(imgFile) }));
+      gifurls.ForEach(imgFile => lbxAllPics.Items.Add(new RI { GifUrl = $"{_urlRoot}{UrlSuffix}/{imgFile}", FileName = Path.GetFileNameWithoutExtension(imgFile) }));      //lbxAllPics.ItemsSource = list;
 
       chkIsPlaying.Content = $"_{UrlSuffix}      {gifurls.Count} files      {list.First().ImgTime:ddd HH:mm} ÷ {list.Last().ImgTime:ddd HH:mm}";
 
@@ -30,20 +34,30 @@ public partial class RadarTypeViewUserControl : UserControl
 
       await Task.Delay(1000);
 
-      var cts = new CancellationTokenSource();
-
       using var timer = new PeriodicTimer(TimeSpan.FromSeconds(_fpsPeriod));
-      var counter = 0;
-      var pause = 25;
+      await RunTimer(timer);
+    }
+    catch (Exception ex) { Hand.Play(); MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+  }
+
+  async Task RunTimer(PeriodicTimer timer)
+  {
+    try
+    {
       var prv = StartPlaying;
       chkIsPlaying.IsChecked = true;
       StartPlaying = prv;
-      while (await timer.WaitForNextTickAsync(cts.Token))
+      var counter = 0;
+      while (await timer.WaitForNextTickAsync(_cts.Token))
       {
-        //if (counter == 555)            cts.Cancel();
-        if (chkIsPlaying.IsChecked != true) return;
+        if (chkIsPlaying.IsChecked != true || counter > _maxFrames)
+        {
+          //_cts.Cancel(); // _need to re-new _cts after this.
+          WriteLine($"-- Cancelling at counter {counter}   {UrlSuffix}.");
+          return;
+        }
 
-        var c = ++counter % (lbxAllPics.Items.Count + pause);
+        var c = ++counter % (lbxAllPics.Items.Count + _pauseFrames);
 
         if (c == lbxAllPics.Items.Count && StartPlaying == "0")
           chkIsPlaying.IsChecked = false;
@@ -51,14 +65,22 @@ public partial class RadarTypeViewUserControl : UserControl
         lbxAllPics.SelectedIndex = c >= lbxAllPics.Items.Count ? lbxAllPics.Items.Count : c;
       }
     }
-    catch (Exception ex) { Hand.Play(); MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+    catch (TaskCanceledException ex) { WriteLine($"++ {ex.Message}       {UrlSuffix}."); Hand.Play(); }
+    catch (Exception ex) { WriteLine($"-- {ex.Message}.   {ex.GetType().Name}."); Hand.Play(); }
   }
 
   public string UrlSuffix { get; set; } = "{name}PRECIPET/GIF/WKR";
   public string PreciTp { get; set; } = "Snow";
   public string StartPlaying { get; set; } = "0";
 
-  void chkIsPlaying_Checked(object sender, RoutedEventArgs e) => StartPlaying = "1";
+  async void chkIsPlaying_Checked(object sender, RoutedEventArgs e)
+  {
+    if (!_loaded) return;
+    StartPlaying = "1";
+    using var timer = new PeriodicTimer(TimeSpan.FromSeconds(_fpsPeriod));
+    await RunTimer(timer);
+  }
+
   void lbxAllPics_GotFocus(object sender, RoutedEventArgs e) => chkIsPlaying.IsChecked = false;
   void lbxAllPics_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
   void lbxAllPics_LostFocus(object sender, RoutedEventArgs e) { }
