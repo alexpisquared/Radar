4a11393cc74574658a1280f9c923b1c09eca4e38
alexpisqueared <alex.pigida@outlook.com>
2023-07-21 17:47:38 -0400

    pau

--- Src/OpenWeaApp/App.xaml.cs
+++ Src/OpenWeaApp/App.xaml.cs
@@ -23,37 +23,17 @@ public partial class App : Application
   public App()
   {
     ServiceCollection services = new();
-    //_ = services.AddTransient<MainPlotOldVM>();
-    _ = services.AddTransient<PlotViewModel>();
-    _ = services.AddSingleton<MainWeaWindow>();
-    _ = services.AddSingleton<MainPlotOldWindow>();
-    _ = services.AddTransient<OpenWea>();
-    _ = services.AddTransient<DbxHelper>();
+
     _ = services.AddSingleton<IBpr, Bpr>();
-
     _ = services.AddSingleton<IConfigurationRoot>(new ConfigurationBuilder().AddUserSecrets<App>().Build());
-
     _ = services.AddSingleton<ILogger>(sp => SeriLogHelper.InitLoggerFactory((@$"C:\Temp\Logs\{Assembly.GetExecutingAssembly().GetName().Name![..5]}.{Environment.UserName[..3]}..log"), "-Verbose -Info +Warning -Error -ErNT -11mb -Infi").CreateLogger<MainWeaWindow>());
-
     _ = services.AddSingleton<SpeechSynth>(sp => SpeechSynth.Factory(
-      sp.GetRequiredService<IConfigurationRoot>()["AppSecrets:MagicSpeech"] ?? throw new ArgumentNullException("########"),
+      sp.GetRequiredService<IConfigurationRoot>()["AppSecrets:MagicSpeech"] ?? throw new ArgumentNullException(nameof(services), "cfg.AppSecrets:MagicSpeech"),
       sp.GetRequiredService<ILogger>()));
 
-    _ = services.AddDbContext<WeatherxContext>(optionsBuilder => //tu: dbcontext connstr https://youtu.be/7OBMhoKieqk?t=505 + https://codedocu.com/details?d=2653&a=9&f=425&d=0  :Project\Manage Connected Svcs !!! 2021-12
-    {
-      try
-      {
-        var cfg = _serviceProvider?.GetRequiredService<IConfigurationRoot>();
+    _ = services.AddSingleton<MainWeaWindow>();
 
-        _ = optionsBuilder.UseSqlServer(cfg?.GetConnectionString("Exprs") ?? throw new ArgumentNullException(nameof(services), "cfg.GetConnectionString('Exprs')"));
-      }
-      catch (Exception ex)
-      {
-        _serviceProvider?.GetRequiredService<ILogger>().LogError(ex, _audit);
-        Clipboard.SetText(ex.Message);
-        _ = MessageBox.Show(ex.Message, "Exception - Clipboarded");
-      }
-    });
+    OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(services);
 
     _serviceProvider = services.BuildServiceProvider(); // LibInit.InitLib(services, _serviceProvider);
   }
---
+++ Src/OpenWeaWpfApp/AppStartHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using DB.WeatherX.PwrTls.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenWeather2022;
+using OpenWeaWpfApp.Helpers;
+
+namespace OpenWeaWpfApp;
+
+public static class AppStartHelper
+{
+
+  public static void InitOpenWeaServices(ServiceCollection services)
+  {
+    _ = services.AddTransient<PlotViewModel>();
+    _ = services.AddTransient<OpenWea>();
+    _ = services.AddTransient<DbxHelper>();
+
+    _ = services.AddDbContext<WeatherxContext>(optionsBuilder => //tu: dbcontext connstr https://youtu.be/7OBMhoKieqk?t=505 + https://codedocu.com/details?d=2653&a=9&f=425&d=0  :Project\Manage Connected Svcs !!! 2021-12
+    {
+      try { _ = optionsBuilder.UseSqlServer(new ConfigurationBuilder().AddUserSecrets<App>().Build()?.GetConnectionString("Exprs") ?? throw new ArgumentNullException(nameof(services), "cfg.GetConnectionString('Exprs')\n\nCheck the app settings!!!")); } //todo: bad: new ConfigurationBuilder().AddUserSecrets<App>().Build()
+      catch (Exception ex) { _ = MessageBox.Show(ex.Message, "Exception in App()"); }
+    });
+  }
+}
