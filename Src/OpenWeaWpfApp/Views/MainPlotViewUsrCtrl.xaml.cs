0fd2c423e50d4f9ae8c2b119c948cd38c0e049f1
alexpisqueared <alex.pigida@outlook.com>
2023-08-02 14:06:20 -0400

    pau

--- Src/OpenWeaWpfApp/Views/MainPlotViewUsrCtrl.xaml.cs
+++ Src/OpenWeaWpfApp/Views/MainPlotViewUsrCtrl.xaml.cs
@@ -1,46 +1,35 @@
 ﻿namespace OpenWeaWpfApp.Views;
-public partial class MainPlotViewUsrCtrl : UserControl
+public partial class MainPlotViewUsrCtrl
 {
-  public MainPlotViewUsrCtrl() => InitializeComponent();
+    public MainPlotViewUsrCtrl() => InitializeComponent();
 
 
 
-  public bool IsActive
-  {
-    get { return (bool)GetValue(IsActiveProperty); }
-    set { SetValue(IsActiveProperty, value); }
-  }
+    public bool IsActive { get { return (bool)GetValue(IsActiveProperty); } set { SetValue(IsActiveProperty, value); } }
+    public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(MainPlotViewUsrCtrl), new PropertyMetadata(default(bool), OnPropertyChanged));
+    static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MainPlotViewUsrCtrl)d).Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Collapsed;
 
-  // Using a DependencyProperty as the backing store for IsActive.  This enables animation, styling, binding, etc...
-  public static readonly DependencyProperty IsActiveProperty =
-      DependencyProperty.Register("IsActive", typeof(bool), typeof(MainPlotViewUsrCtrl), new PropertyMetadata(default(bool), OnPropertyChanged));
-
-  static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => 
-    ((MainPlotViewUsrCtrl)d).Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Collapsed;
-
-  void OnLoaded(object sender, RoutedEventArgs e)
-  {
-    if (DesignerProperties.GetIsInDesignMode(this)) return; //tu: design mode
-
-    try
+    void OnLoaded(object sender, RoutedEventArgs e)
     {
-      // Assign DataContext to the viewmodel registered in AppStartHelper.cs OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(IServiceCollection services) method.
-      dynamic ac = Application.Current;
-      var sp = ac.ServiceProvider;
-      DataContext = sp.GetService(typeof(PlotViewModel));
-      
-      ((PlotViewModel?)DataContext)?.PopulateAll("Silent");  // only lines chart is drawn.
+        if (DesignerProperties.GetIsInDesignMode(this)) return; //tu: design mode
+
+        try
+        {
+            // Assign DataContext to the viewmodel registered in AppStartHelper.cs OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(IServiceCollection services) method.
+            DataContext = (Application.Current as dynamic).ServiceProvider.GetService(typeof(PlotViewModel));
+
+            ((PlotViewModel?)DataContext)?.PopulateAll("Silent");  // only lines chart is drawn.
+        }
+        catch (Exception ex) { ex.Pop(); }
     }
-    catch (Exception ex)    {      ex.Pop();    }
-  }
-  void OnPoplte(object sender, RoutedEventArgs e) => ((PlotViewModel)DataContext).PopulateAll(null);  // only lines chart is drawn.
-  void OnPocBin(object sender, RoutedEventArgs e) => new PocBin().Show();
-  void OnDragMove(object s, MouseButtonEventArgs e)
-  {
-    if (e.LeftButton != MouseButtonState.Pressed) return;
+    void OnPoplte(object sender, RoutedEventArgs e) => ((PlotViewModel)DataContext).PopulateAll(null);  // only lines chart is drawn.
+    void OnPocBin(object sender, RoutedEventArgs e) => new PocBin().Show();
+    void OnDragMove(object s, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton != MouseButtonState.Pressed) return;
 
-    this.FindParentWindow().DragMove();
+        this.FindParentWindow().DragMove();
 
-    e.Handled = true;
-  }
+        e.Handled = true;
+    }
 }
