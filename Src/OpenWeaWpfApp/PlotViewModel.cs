41bcc23e70c7a826a6cb2944df94b0c3d1b3a2e0
alexpisqueared <alex.pigida@outlook.com>
2023-08-05 17:49:56 -0400

    logging to the eventlog...

--- Src/OpenWeaWpfApp/PlotViewModel.cs
+++ Src/OpenWeaWpfApp/PlotViewModel.cs
@@ -105,7 +105,7 @@ public partial class PlotViewModel : ObservableValidator
     _lgr.Log(LogLevel.Trace, "PlotViewModel() EOCtor");
 
     //if (VersionHelper.IsDbg) _synth.SpeakFAF("Are you sure?", voice: "en-US-AriaNeural", style: "terrified", speakingRate: .25, volumePercent: 75);
-    if (VersionHelper.IsDbg) _synth.SpeakFAF("Done!");
+    //if (VersionHelper.IsDbg) _synth.SpeakFAF("Done!");
   }
 
   [ObservableProperty] DateTime lastBuild = VersionHelper.GetLastBuildTime;
