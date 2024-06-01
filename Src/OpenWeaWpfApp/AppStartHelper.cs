53cad70e12a8f5ca003497d83b1474091489623b
alexpisqueared <alex.pigida@outlook.com>
2024-05-31 20:15:09 -0400

    serviceProvider.GetRequiredService<IConfigurationRoot>(); // a better way (May 2024)

--- Src/OpenWeaWpfApp/AppStartHelper.cs
+++ Src/OpenWeaWpfApp/AppStartHelper.cs
@@ -8,16 +8,17 @@ public static class AppStartHelper
     _ = services.AddTransient<OpenWea>();
     _ = services.AddTransient<DbxHelper>();
 
-    _ = services.AddDbContext<WeatherxContext>(optionsBuilder => //tu: dbcontext connstr https://youtu.be/7OBMhoKieqk?t=505 + https://codedocu.com/details?d=2653&a=9&f=425&d=0  :Project\Manage Connected Svcs !!! 2021-12
+    _ = services.AddDbContext<WeatherxContext>((serviceProvider, optionsBuilder) => //tu: dbcontext connstr https://youtu.be/7OBMhoKieqk?t=505 + https://codedocu.com/details?d=2653&a=9&f=425&d=0  :Project\Manage Connected Svcs !!! 2021-12
     {
       try
       {
-        IConfigurationRoot cfg = (Application.Current as dynamic).ServiceProvider.GetService(typeof(IConfigurationRoot)); 
+        IConfigurationRoot cfg = //(Application.Current as dynamic).ServiceProvider.GetService(typeof(IConfigurationRoot)); 
+                                                                    serviceProvider.GetRequiredService<IConfigurationRoot>(); // a better way (May 2024)
 
         var scs = cfg.GetConnectionString("Exprs") ?? throw new ArgumentNullException(nameof(services), $"cfg.GetConnectionString('Exprs')\n\nCheck the app settings!!!\n\n{cfg["WhereAmI"]}\n\n");
 
         _ = optionsBuilder.UseSqlServer(scs);
-      } 
+      }
       catch (Exception ex) { ex.Pop(); }
     });
   }
