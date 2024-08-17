24a00be36f9d3a1ad055eaf77f01246bc98ed381
alexpisqueared <alex.pigida@outlook.com>
2024-08-17 17:13:24 -0400

    ;lkj

--- Src/Radar/View/RadarAnimation.xaml.cs
+++ Src/Radar/View/RadarAnimation.xaml.cs
@@ -1,32 +1,27 @@
-using System;
-using System.Threading.Tasks;
-using System.Windows;
+namespace Radar;
 
-namespace Radar
+public partial class RadarAnimation //: AAV.WPF.Base.WindowBase
 {
-  public partial class RadarAnimation //: AAV.WPF.Base.WindowBase
+  public RadarAnimation(double alarmThreshold = .05)
   {
-    public RadarAnimation(double alarmThreshold = .05)
+    InitializeComponent();
+
+    if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1] == "ModeH")
+      Hide();
+
+    Loaded += async (s, e) =>
     {
-      InitializeComponent();
-
-      if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1] == "ModeH")
-        Hide();
-
-      Loaded += async (s, e) =>
+      if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1] == "ShowIfOnOrComingSoon")
       {
-        if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1] == "ShowIfOnOrComingSoon")
-        {
-          await Task.Delay(180000);
+        await Task.Delay(180000);
 
-          Close();
-          System.Diagnostics.Trace.WriteLine($"{DateTime.Now:yy.MM.dd HH:mm:ss.f} => ::>Application.Current.Shutdown();g");
-          Application.Current.Shutdown();
-        }
-      };
+        Close();
+        //WriteLine($"{DateTime.Now:yy.MM.dd HH:mm:ss.f} => ::>Application.Current.Shutdown();g");
+        Application.Current.Shutdown();
+      }
+    };
 
-      ruc1.AlarmThreshold = alarmThreshold;
-      ruc1.Focus();
-    }
+    ruc1.AlarmThreshold = alarmThreshold;
+    _ = ruc1.Focus();
   }
 }
