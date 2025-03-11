7db8b18722e259754f62d3dc413c75aace90c6fe
alexpisqueared <alex.pigida@outlook.com>
2025-03-10 22:50:41 -0400

    pau

--- Src/OutlookCrashMonitor/App.xaml.cs
+++ Src/OutlookCrashMonitor/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace OutlookCrashMonitor;
 public partial class App : Application
 {
+  long started = Stopwatch.GetTimestamp();
   protected override void OnExit(ExitEventArgs e)
   {
     base.OnExit(e);
-    System.IO.File.AppendAllText(@"C:\temp\temp.txt", $"{DateTime.Now:HH:mm:ss} ■\n");
+    System.IO.File.AppendAllText(@"C:\temp\temp.txt", $"{DateTime.Now:HH:mm:ss}  {Stopwatch.GetElapsedTime(started):mmss} ■\n");
   }
 }
 
