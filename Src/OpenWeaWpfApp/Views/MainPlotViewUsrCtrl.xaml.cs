c28c1c89e1a1e915ce221e795a78fbb710d4db2c
AlexPi <alex.pigida@outlook.com>
2026-03-07 21:01:31 -0500

    NuGet versions updated

--- Src/OpenWeaWpfApp/Views/MainPlotViewUsrCtrl.xaml.cs
+++ Src/OpenWeaWpfApp/Views/MainPlotViewUsrCtrl.xaml.cs
@@ -1,34 +1,35 @@
 ﻿namespace OpenWeaWpfApp.Views;
+
 public partial class MainPlotViewUsrCtrl
 {
-    public MainPlotViewUsrCtrl() => InitializeComponent();
+  public MainPlotViewUsrCtrl() => InitializeComponent();
 
+  public bool IsActive { get => (bool)GetValue(IsActiveProperty); set => SetValue(IsActiveProperty, value); }
+  public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(MainPlotViewUsrCtrl), new PropertyMetadata(default(bool), OnPropertyChanged));
+  static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MainPlotViewUsrCtrl)d).Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Collapsed;
 
+  void OnLoaded(object sender, RoutedEventArgs e)
+  {
+    if (DesignerProperties.GetIsInDesignMode(this)) return; //tu: design mode
 
-    public bool IsActive { get { return (bool)GetValue(IsActiveProperty); } set { SetValue(IsActiveProperty, value); } }
-    public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(MainPlotViewUsrCtrl), new PropertyMetadata(default(bool), OnPropertyChanged));
-    static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MainPlotViewUsrCtrl)d).Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Collapsed;
-
-    void OnLoaded(object sender, RoutedEventArgs e)
+    try
     {
-        if (DesignerProperties.GetIsInDesignMode(this)) return; //tu: design mode
+      // Assign DataContext to the viewmodel registered in AppStartHelper.cs OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(IServiceCollection services) method.
+      DataContext = (System.Windows.Application.Current as dynamic).ServiceProvider.GetService(typeof(PlotViewModel));
 
-        try
-        {
-            // Assign DataContext to the viewmodel registered in AppStartHelper.cs OpenWeaWpfApp.AppStartHelper.InitOpenWeaServices(IServiceCollection services) method.
-            DataContext = (System.Windows.Application.Current as dynamic).ServiceProvider.GetService(typeof(PlotViewModel));
-
-            ((PlotViewModel?)DataContext)?.PopulateAll("Silent");  // only lines chart is drawn.
-        }
-        catch (Exception ex) { ex.Pop(); }
+      _ = (((PlotViewModel?)DataContext)?.PopulateAll("Silent"));  // only lines chart is drawn.
     }
-    async void OnPoplte(object sender, RoutedEventArgs e) => await ((PlotViewModel)DataContext).PopulateAll(null);  // only lines chart is drawn.
-    void OnDragMove(object s, MouseButtonEventArgs e)
-    {
-        if (e.LeftButton != MouseButtonState.Pressed) return;
+    catch (Exception ex) { ex.Pop(); }
+  }
+  async void OnPoplte(object sender, RoutedEventArgs e) => await ((PlotViewModel)DataContext).PopulateAll(null);  // only lines chart is drawn.
+  void OnDragMove(object s, MouseButtonEventArgs e)
+  {
+    if (e.LeftButton != MouseButtonState.Pressed) return;
 
-        this.FindParentWindow().DragMove();
+    this.FindParentWindow().DragMove();
 
-        e.Handled = true;
-    }
+    e.Handled = true;
+  }
+
+  private void OnClose(object sender, RoutedEventArgs e) => this.FindParentWindow().Close();
 }
